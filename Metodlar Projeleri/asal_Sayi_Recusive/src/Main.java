6d22c9c346ef6de250e2b4ff751d4188ff6e2f6b
Sinan <sinan_karabulut97@hotmail.com>
2023-09-19 10:22:02 +0300

    Upload Code

--- Metodlar Projeleri/asal_Sayi_Recusive/src/Main.java
+++ Metodlar Projeleri/asal_Sayi_Recusive/src/Main.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
-    static void asal(int a ,int b ) {
-        if(a>0) {
-            return
+    static boolean isAsal(int sayi, int sinir) {
+        if (sayi < 2) {
+            return true;
+        }
+        if (sinir == 1) {
+            return true;
+        }
+        if (sayi % sinir == 0) {
+            return false;
+        }
+        return isAsal(sayi, sinir - 1);
+
     }
 
-
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz: ");
+        int sayi = scan.nextInt();
+
+        if (isAsal(sayi, sayi / 2)) {
+            System.out.println(sayi+" Asal Sayıdır.");
+        }
+        else{
+            System.out.println(sayi+" Asal Değildir.");
+        }
+
 
 
     }
