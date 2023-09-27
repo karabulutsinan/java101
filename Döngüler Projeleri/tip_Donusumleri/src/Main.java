f520b64e77c80d2a940c43fa93fea648cda43757
Sinan <sinan_karabulut97@hotmail.com>
2023-09-27 10:40:20 +0300

    Upload Code

--- Döngüler Projeleri/tip_Donusumleri/src/Main.java
+++ Döngüler Projeleri/tip_Donusumleri/src/Main.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+       /* Scanner input = new Scanner(System.in);
         System.out.print("Tam Sayı Giriniz :");
-        int n =input.nextInt();
-        double myDouble =n;
-        System.out.println("Girdiğin tam sayının Double Değeri: "+myDouble);
+        int n = input.nextInt();
+        double myDouble = n;
+        System.out.println("Girdiğin tam sayının Double Değeri: " + myDouble);
 
         System.out.print("\nOndalıklı Sayı Giriniz :");
         double k = input.nextDouble();
-        int myInteger= (int) k;
-        System.out.println("Girdiğin tam sayının Double Değeri: "+myInteger);
+        int myInteger = (int) k;
+       System.out.println("Girdiğin tam sayının Double Değeri: " + myInteger);
+*/
+        System.out.println("-------------------------------------------------");
+
+        int[] dizi = {1, 2, 3, 4, 5, 6};
+        System.out.println("ilk  değer : " + dizi[0]);
+        degistir(dizi);
+        System.out.println("son değer : "+dizi[0]);
 
 
 
-
-
+    }
+    static void degistir ( int[] dizi ){
+        dizi[0]=dizi[0]+10;
     }
 }
